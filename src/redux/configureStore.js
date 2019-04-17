6fa16098997fd5eb5bf5e3f8a6ca0b637120bb5e
Lindsey Jelepis <lin.jelepis@gmail.com>
2019-04-16 18:42:23 -0700

    React Redux Forms Revisited

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,19 @@
 import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
 
 //reducer and initialState was deleted inside createStore,
 //because all we need is combineReducers
 //inside this combineReducers we specify how to combine these four simpler Reducers we have defined up there.
 //After adding 4, we recomposed the global state for our application.
+//React redux form fills in all the details by itself, so we don't need reducers.
+//even if you reload your component, your form will stay the same.
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -17,7 +21,10 @@ export const ConfigureStore = () => {
             dishes: Dishes,
             comments: Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
